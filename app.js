8598bb53d4c01bb8f870d08dc4579ba9dba84264
johnnyfwk <johnnyfong@live.com>
2023-07-21 07:58:31 +0100

    First commit.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 // const {
 //     getUsers
 // } = require("./controllers/users.controller")
@@ -8,7 +10,23 @@ const app = express();
 // app.get("/api/users", getUsers);
 
 app.get("/api/users", (request, response) => {
-    response.status(200).send({ msg: "These are the users." })
+    response.status(200).send({ msg: "These are all the users." })
 });
+
+app.get("/api/users/:user_id", (request, response) => {
+    response.status(200).send({ msg: "This is a single user." })
+})
+
+app.get("/api/users?hungry=true", (request, response) => {
+    const { hungry } = request.query;
+    console.log(hungry);
+    console.log(request.query)
+    response.status(200).send({ msg: hungry })
+})
+
+app.post("/api/users", (request, response) => {
+    console.log(request.body);
+    response.status(201).send({ msg: request.body })
+})
 
 module.exports = app;
---
+++ database/create/create-database.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE help_me_choose_one
