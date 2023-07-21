785728bf0ba2059a9cbc7b07e0abcb6561ea1e72
johnnyfwk <johnnyfong@live.com>
2023-07-21 17:24:03 +0100

    posts.js: Added image element for each option to optionsAndVotes nested array.

--- database/data/posts.js
+++ database/data/posts.js
@@ -5,10 +5,10 @@ const posts = [
         title: "Which crisp flavour should I have right now?",
         description: "I have the choice between salt and vinegar, cheese and onion, ready salted, or prawn cocktail.",
         optionsAndVotes: [
-            ["salt and vinegar", [2, 8]],
-            ["cheese and onion", [6]],
-            ["ready salted", [3, 7, 9, 10]],
-            ["prawn cocktail", [4, 5]]
+            ["salt and vinegar", "https://www.rd.com/wp-content/uploads/2019/09/GettyImages-621924830-scaled.jpg", [2, 8]],
+            ["cheese and onion", "https://ichef.bbci.co.uk/news/1536/cpsprodpb/F132/production/_120364716_13efa5e0-90a6-4f69-a7fb-7df6d3e027ca.jpg", [6]],
+            ["ready salted", "https://www.rd.com/wp-content/uploads/2019/09/GettyImages-621924830-scaled.jpg", [3, 7, 9, 10]],
+            ["prawn cocktail", "https://ichef.bbci.co.uk/news/1536/cpsprodpb/F132/production/_120364716_13efa5e0-90a6-4f69-a7fb-7df6d3e027ca.jpg", [4, 5]]
         ],
         postOwnerId: 1
     },
@@ -18,10 +18,10 @@ const posts = [
         title: "What colour should I paint my bedroom?",
         description: "Going to repaint my bedroom this weekend. What colour should I paint it?",
         optionsAndVotes: [
-            ["beige", [1, 3, 4, 7]],
-            ["black", []],
-            ["pink", [5, 8, 9, 10]],
-            ["orange", [6]]
+            ["beige", "https://www.rd.com/wp-content/uploads/2019/09/GettyImages-621924830-scaled.jpg", [1, 3, 4, 7]],
+            ["black", "https://ichef.bbci.co.uk/news/1536/cpsprodpb/F132/production/_120364716_13efa5e0-90a6-4f69-a7fb-7df6d3e027ca.jpg", []],
+            ["pink", "https://www.rd.com/wp-content/uploads/2019/09/GettyImages-621924830-scaled.jpg", [5, 8, 9, 10]],
+            ["orange", "https://ichef.bbci.co.uk/news/1536/cpsprodpb/F132/production/_120364716_13efa5e0-90a6-4f69-a7fb-7df6d3e027ca.jpg", [6]]
         ],
         postOwnerId: 2
     },
@@ -31,8 +31,8 @@ const posts = [
         title: "Which mobile phone should I buy?",
         description: "I can't choose between the Google Pixel 10 Pro or Apple iPhone 30.",
         optionsAndVotes: [
-            ["Gooogle Pixel 10 Pro", [1, 2, 5, 9, 10]],
-            ["Apple iPhone 30", [4, 6, 7, 8]]
+            ["Gooogle Pixel 10 Pro", "https://www.rd.com/wp-content/uploads/2019/09/GettyImages-621924830-scaled.jpg", [1, 2, 5, 9, 10]],
+            ["Apple iPhone 30", "https://ichef.bbci.co.uk/news/1536/cpsprodpb/F132/production/_120364716_13efa5e0-90a6-4f69-a7fb-7df6d3e027ca.jpg", [4, 6, 7, 8]]
         ],
         postOwnerId: 3
     },
@@ -42,10 +42,10 @@ const posts = [
         title: "Which Tesco meal deal sandwich should I have for lunch?",
         description: "I'm going to take an extra long lunch break today and enjoy a sandwich from Tesco. Which one is the best?",
         optionsAndVotes: [
-            ["BLT", [5, 6, 7, 8]],
-            ["Tuna & Cucumber", [3]],
-            ["Chicken Salad", [1, 9, 10]],
-            ["prawn mayonnaise", [2]]
+            ["BLT", "https://www.rd.com/wp-content/uploads/2019/09/GettyImages-621924830-scaled.jpg", [5, 6, 7, 8]],
+            ["Tuna & Cucumber", "https://ichef.bbci.co.uk/news/1536/cpsprodpb/F132/production/_120364716_13efa5e0-90a6-4f69-a7fb-7df6d3e027ca.jpg", [3]],
+            ["Chicken Salad", "https://www.rd.com/wp-content/uploads/2019/09/GettyImages-621924830-scaled.jpg", [1, 9, 10]],
+            ["prawn mayonnaise", "https://ichef.bbci.co.uk/news/1536/cpsprodpb/F132/production/_120364716_13efa5e0-90a6-4f69-a7fb-7df6d3e027ca.jpg", [2]]
         ],
         postOwnerId: 4
     },
@@ -55,10 +55,10 @@ const posts = [
         title: "Where should I go on holiday this summer?",
         description: "I have 2 weeks off work this summer and can't decide where to go. Where should I go?",
         optionsAndVotes: [
-            ["Scotland", [3]],
-            ["France", [7, 8]],
-            ["Spain", [2, 4, 9]],
-            ["Italy", [1, 6, 10]]
+            ["Scotland", "", [3]],
+            ["France", "", [7, 8]],
+            ["Spain", "", [2, 4, 9]],
+            ["Italy", "", [1, 6, 10]]
         ],
         postOwnerId: 5
     },
@@ -68,11 +68,11 @@ const posts = [
         title: "How many kids should I have?",
         description: "My husband and I want to have children but don't know how many we should we have. What do you guys think?",
         optionsAndVotes: [
-            ["1", [2, 3, 7, 10]],
-            ["2", [1, 4, 9]],
-            ["3", [5]],
-            ["4", []],
-            ["10", [8]]
+            ["1", "", [2, 3, 7, 10]],
+            ["2", "", [1, 4, 9]],
+            ["3", "", [5]],
+            ["4", "", []],
+            ["10", "", [8]]
         ],
         postOwnerId: 6
     },
@@ -81,9 +81,9 @@ const posts = [
         title: "What headphones should I buy?",
         description: "I just broke my headphones and need a new pair. Which one should I buy?",
         optionsAndVotes: [
-            ["Bose QuietComfort 45", [1, 2, 3, 4, 8, 10]],
-            ["Sony WH-1000XM4", [9]],
-            ["Sennheiser Momentum 4 Wireless", [5, 6]]
+            ["Bose QuietComfort 45", "https://www.rd.com/wp-content/uploads/2019/09/GettyImages-621924830-scaled.jpg", [1, 2, 3, 4, 8, 10]],
+            ["Sony WH-1000XM4", "https://ichef.bbci.co.uk/news/1536/cpsprodpb/F132/production/_120364716_13efa5e0-90a6-4f69-a7fb-7df6d3e027ca.jpg", [9]],
+            ["Sennheiser Momentum 4 Wireless", "https://www.rd.com/wp-content/uploads/2019/09/GettyImages-621924830-scaled.jpg", [5, 6]]
         ],
         postOwnerId: 7
     },
@@ -93,9 +93,9 @@ const posts = [
         title: "What car should I buy?",
         description: "I am super rich and feel like splashing some cash on a car tomorrow. Which one should I buy?",
         optionsAndVotes: [
-            ["Ford Fiesta", []],
-            ["Tesla Model 3", [1, 2, 3, 4, 5, 6, 9, 10]],
-            ["BMW 3 Series", [7]]
+            ["Ford Fiesta", "https://www.rd.com/wp-content/uploads/2019/09/GettyImages-621924830-scaled.jpg", []],
+            ["Tesla Model 3", "https://ichef.bbci.co.uk/news/1536/cpsprodpb/F132/production/_120364716_13efa5e0-90a6-4f69-a7fb-7df6d3e027ca.jpg", [1, 2, 3, 4, 5, 6, 9, 10]],
+            ["BMW 3 Series", "https://www.rd.com/wp-content/uploads/2019/09/GettyImages-621924830-scaled.jpg", [7]]
         ],
         postOwnerId: 8
     },
@@ -105,10 +105,10 @@ const posts = [
         title: "Which bank should I open a bank account with?",
         description: "I need a bank account but don't know which bank to open one with? Can I get some suggestions please? Thanks",
         optionsAndVotes: [
-            ["Monzo", [1, 2, 3, 4, 6]],
-            ["Starling", [5, 7, 10]],
-            ["Halifax", [8]],
-            ["Barclays", []]
+            ["Monzo", "https://www.rd.com/wp-content/uploads/2019/09/GettyImages-621924830-scaled.jpg", [1, 2, 3, 4, 6]],
+            ["Starling", "https://ichef.bbci.co.uk/news/1536/cpsprodpb/F132/production/_120364716_13efa5e0-90a6-4f69-a7fb-7df6d3e027ca.jpg", [5, 7, 10]],
+            ["Halifax", "https://www.rd.com/wp-content/uploads/2019/09/GettyImages-621924830-scaled.jpg", [8]],
+            ["Barclays", "https://ichef.bbci.co.uk/news/1536/cpsprodpb/F132/production/_120364716_13efa5e0-90a6-4f69-a7fb-7df6d3e027ca.jpg", []]
         ],
         postOwnerId: 9
     },
@@ -118,9 +118,9 @@ const posts = [
         title: "What should I have for my dinner tonight?",
         description: "Can't choose between Mcdonald's, Burger King, and KFC.",
         optionsAndVotes: [
-            ["McDonalds", [2, 3, 4, 5, 6]],
-            ["Burger King", [1, 8]],
-            ["KFC", [7, 9]]
+            ["McDonalds", "", [2, 3, 4, 5, 6]],
+            ["Burger King", "", [1, 8]],
+            ["KFC", "", [7, 9]]
         ],
         postOwnerId: 10
     }
