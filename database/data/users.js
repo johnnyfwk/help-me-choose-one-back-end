eb42f9617810f89ebe39ae66b9f1b8775df6a727
johnnyfwk <johnnyfong@live.com>
2023-07-22 15:21:57 +0100

    users.js: Capitalised some usernames for sign up testing.

--- database/data/users.js
+++ database/data/users.js
@@ -1,61 +1,61 @@
 const users = [
     {
-        username: "johnny",
-        password: "internick",
+        username: "Johnny",
+        password: "password",
         avatar: "default-avatar.webp",
         joinDate: "2023-07-21T00:59:42.783Z"
     },
     {
-        username: "abbie",
-        password: "internick",
+        username: "Abbie",
+        password: "password",
         avatar: "default-avatar.webp",
         joinDate: "2023-07-21T01:59:42.783Z"
     },
     {
         username: "billy",
-        password: "internick",
+        password: "password",
         avatar: "default-avatar.webp",
         joinDate: "2023-07-21T02:59:42.783Z"
     },
     {
-        username: "catherine",
-        password: "internick",
+        username: "CATHERINE",
+        password: "password",
         avatar: "default-avatar.webp",
         joinDate: "2023-07-21T03:59:42.783Z"
     },
     {
-        username: "david",
-        password: "internick",
+        username: "DaViD",
+        password: "password",
         avatar: "default-avatar.webp",
         joinDate: "2023-07-21T04:59:42.783Z"
     },
     {
         username: "ellie",
-        password: "internick",
+        password: "password",
         avatar: "default-avatar.webp",
         joinDate: "2023-07-21T05:59:42.783Z"
     },
     {
-        username: "frank",
-        password: "internick",
+        username: "FRANK",
+        password: "password",
         avatar: "default-avatar.webp",
         joinDate: "2023-07-21T06:59:42.783Z"
     },
     {
         username: "gary",
-        password: "internick",
+        password: "password",
         avatar: "default-avatar.webp",
         joinDate: "2023-07-21T07:59:42.783Z"
     },
     {
-        username: "helen",
-        password: "internick",
+        username: "HeleN",
+        password: "password",
         avatar: "default-avatar.webp",
         joinDate: "2023-07-21T08:59:42.783Z"
     },
     {
         username: "ian",
-        password: "internick",
+        password: "password",
         avatar: "default-avatar.webp",
         joinDate: "2023-07-21T09:59:42.783Z"
     },
