9f1e50435600056e52b0f5e416ebc6c1c9528bdc
johnnyfwk <johnnyfong@live.com>
2023-07-30 08:57:12 +0100

    Changed path of endpoints.

--- app.js
+++ app.js
@@ -50,34 +50,34 @@ app.use(cors());
 
 app.use(express.json());
 
-app.get("/api/users", getUsers);
-app.get("/api/users/:user_id", getUserById);
-app.post("/api/users", addUser);
-app.patch("/api/users/:user_id", updateUserById);
-app.delete("/api/users/:user_id", deleteUserById);
+app.get("/bE2uT8XzAqG1yJ6fNvL3/users", getUsers);
+app.get("/bE2uT8XzAqG1yJ6fNvL3/users/:user_id", getUserById);
+app.post("/bE2uT8XzAqG1yJ6fNvL3/users", addUser);
+app.patch("/bE2uT8XzAqG1yJ6fNvL3/users/:user_id", updateUserById);
+app.delete("/bE2uT8XzAqG1yJ6fNvL3/users/:user_id", deleteUserById);
 
-app.get("/api/posts", getPosts);
-app.get("/api/posts/:post_id", getPostById);
-app.get("/api/posts/category/:category_name", getPostsByCategory);
-app.get("/api/users/:user_id/posts", getPostsByUserId);
-app.post("/api/posts", createPost);
-app.patch("/api/posts/:post_id", updatePostById);
-app.delete("/api/posts/:post_id", deletePostById);
-app.delete("/api/users/:user_id/posts", deletePostsByUserId);
+app.get("/bE2uT8XzAqG1yJ6fNvL3/posts", getPosts);
+app.get("/bE2uT8XzAqG1yJ6fNvL3/posts/:post_id", getPostById);
+app.get("/bE2uT8XzAqG1yJ6fNvL3/posts/category/:category_name", getPostsByCategory);
+app.get("/bE2uT8XzAqG1yJ6fNvL3/users/:user_id/posts", getPostsByUserId);
+app.post("/bE2uT8XzAqG1yJ6fNvL3/posts", createPost);
+app.patch("/bE2uT8XzAqG1yJ6fNvL3/posts/:post_id", updatePostById);
+app.delete("/bE2uT8XzAqG1yJ6fNvL3/posts/:post_id", deletePostById);
+app.delete("/bE2uT8XzAqG1yJ6fNvL3/users/:user_id/posts", deletePostsByUserId);
 
-app.get("/api/comments", getComments);
-app.get("/api/comments/:comment_id", getCommentById);
-app.get("/api/posts/:post_id/comments", getCommentsByPostId);
-app.get("/api/users/:user_id/comments", getCommentsByUserId);
-app.post("/api/comments", createComment);
-app.patch("/api/comments/:comment_id", updateCommentById);
-app.delete("/api/comments/:comment_id", deleteCommentById);
-app.delete("/api/posts/:post_id/comments", deleteCommentsByPostId);
-app.delete("/api/users/:user_id/comments", deleteCommentsByUserId);
+app.get("/bE2uT8XzAqG1yJ6fNvL3/comments", getComments);
+app.get("/bE2uT8XzAqG1yJ6fNvL3/comments/:comment_id", getCommentById);
+app.get("/bE2uT8XzAqG1yJ6fNvL3/posts/:post_id/comments", getCommentsByPostId);
+app.get("/bE2uT8XzAqG1yJ6fNvL3/users/:user_id/comments", getCommentsByUserId);
+app.post("/bE2uT8XzAqG1yJ6fNvL3/comments", createComment);
+app.patch("/bE2uT8XzAqG1yJ6fNvL3/comments/:comment_id", updateCommentById);
+app.delete("/bE2uT8XzAqG1yJ6fNvL3/comments/:comment_id", deleteCommentById);
+app.delete("/bE2uT8XzAqG1yJ6fNvL3/posts/:post_id/comments", deleteCommentsByPostId);
+app.delete("/bE2uT8XzAqG1yJ6fNvL3/users/:user_id/comments", deleteCommentsByUserId);
 
-app.get("/api/reports", getReports);
-app.get("/api/reports/:report_id", getReportById);
-app.post("/api/reports", createReport);
+app.get("/bE2uT8XzAqG1yJ6fNvL3/reports", getReports);
+app.get("/bE2uT8XzAqG1yJ6fNvL3/reports/:report_id", getReportById);
+app.post("/bE2uT8XzAqG1yJ6fNvL3/reports", createReport);
 
 app.all("*", handle404Errors);
 
